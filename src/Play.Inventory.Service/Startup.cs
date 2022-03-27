956c7082506cf6cbbd006b174a76972f0c2ab8e2
docteurjay <aristejean@gmail.com>
2022-03-26 23:03:57 -0400

    Adding the CORS Middleware

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -19,12 +19,14 @@ namespace Play.Inventory.Service
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        private const string AllowedOriginSetting = "AllowedOrigin";
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -55,6 +57,13 @@ namespace Play.Inventory.Service
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
+           
+                app.UseCors(builder => 
+               {
+                   builder.WithOrigins(Configuration[AllowedOriginSetting])
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+               });
             }
 
             app.UseHttpsRedirection();
