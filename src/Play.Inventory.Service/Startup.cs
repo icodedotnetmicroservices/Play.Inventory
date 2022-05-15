369e8aabcda35cfa2b6a24b11427956809f79f29
docteurjay <aristejean@gmail.com>
2022-05-14 23:59:54 -0400

    Added The Health Checks

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Play.Common.HealthChecks;
 using Play.Common.Identity;
 using Play.Common.MassTransit;
 using Play.Common.MongoDB;
@@ -54,6 +55,9 @@ namespace Play.Inventory.Service
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Inventory.Service", Version = "v1" });
             });
+
+            services.AddHealthChecks()
+                    .AddMongoDb();
         }
 
 
@@ -85,6 +89,7 @@ namespace Play.Inventory.Service
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapPlayEconomyHealthChecks();
             });
         }
         private static void AddCatalogClient(IServiceCollection services)
